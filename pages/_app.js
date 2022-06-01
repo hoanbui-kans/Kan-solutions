df6d1950c137aaa660da0409ee876900d93e8402
tonybui98 <96325836+tonybui98@users.noreply.github.com>
2022-06-01 23:36:54 +0700

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import 'rsuite/dist/rsuite.min.css'; // or 'rsuite/dist/rsuite.min.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header/>
+      <Component {...pageProps} />
+    <Footer />
+    </>
+  )
+  
+ 
 }
  
 export default MyApp
